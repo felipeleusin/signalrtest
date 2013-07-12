4b6319fcfec43afee30de4a44272836c4275d8c0
Felipe Amorim <felipe.leusin@gmail.com>
2013-07-12 18:24:49 -0300

    added source

---
+++ WebApplication2/Startup.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Nancy;
+using Owin;
+
+namespace WebApplication2
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var hubConf = new HubConfiguration()
+            {
+                EnableCrossDomain = true,
+                EnableDetailedErrors = true
+            };
+
+            app.MapHubs(hubConf).UseNancy(new DefaultNancyBootstrapper());
+        }
+    }
+
+    public class SimpleHub : Hub
+    {
+        public void Message(string message)
+        {
+            Clients.All.message(message);
+        }
+    }
+
+    public class PingModule : NancyModule
+    {
+        public PingModule()
+        {
+            Get["/"] = _ => "Ping!";
+        }
+    }
+}
